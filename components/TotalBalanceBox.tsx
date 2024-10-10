59d16701dc2355dafb11f1872f0b6b15b8af79f7
arnav2802 <arnav171103@gmail.com>
2024-10-10 18:57:30 +0530

    modified files

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TotalBalanceBox = ({ accounts = [] , totalBanks, totalCurrentBalance }: TotalBalanceBoxProps ) => {
+  return (
+    <section className="total-balance">
+        <div className = "total-balance-chart">
+            
+        </div>
+
+        <div>
+
+        </div>
+    </section>
+  )
+}
+
+export default TotalBalanceBox
