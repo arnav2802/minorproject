8b9dc56181b0bb56340d18557064395e08f3b2ce
arnav2802 <arnav171103@gmail.com>
2024-10-10 23:05:52 +0530

    modified files

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const TotalBalanceBox = ({ accounts = [] , totalBanks, totalCurrentBalance }: TotalBalanceBoxProps ) => {
   return (
     <section className="total-balance">
@@ -7,8 +5,22 @@ const TotalBalanceBox = ({ accounts = [] , totalBanks, totalCurrentBalance }: To
             
         </div>
 
-        <div>
+        <div className="flex flex-col gap-6">
+          <h2 className="header-2">
+           Bank Accounts: {totalBanks}  
+          </h2>
+          <div className="flex flex-col gap-2">
+            <p className="total-balance-label">
+              Total current Balance
+            </p>
 
+            <p className="total-balance-amount flex-center gap-2">
+              {totalCurrentBalance}
+
+            </p>
+
+          </div>
+      
         </div>
     </section>
   )
