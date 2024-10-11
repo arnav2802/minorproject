90a73a160296192e877d13cd1ffd5a69f8797452
arnav2802 <arnav171103@gmail.com>
2024-10-11 15:12:53 +0530

    modified files

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,4 +1,4 @@
-'use-client'
+'use client'
 import { Doughnut } from "react-chartjs-2"
 import {Chart as ChartJS, ArcElement, Tooltip, Legend} from "chart.js"
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -7,12 +7,13 @@ const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
     const data = {
         datasets:[{
             label: 'Banks',
-            data: ['#0747b6','#2265db','#2f91fa'],
-            backgroundColor: ['']
-        }]
+            data: [1520,5200,3750],
+            backgroundColor: ['#0747b6','#2265db','#2f91fa']
+        }],
+        labels: ['Bank 1','bank 2','Bank 3']
     }
   return (
-    <Doughnut data={} />
+    <Doughnut data={data} />
   )
 }
 
